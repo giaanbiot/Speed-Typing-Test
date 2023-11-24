3b6ebd1f29bf8124d03d595542af4275bd41b338
giaanbiot <151908352+giaanbiot@users.noreply.github.com>
2023-11-24 15:13:48 +0800

    Add files via upload

---
+++ paragraphs.js
@@ -0,0 +1,5 @@
+const paragraphs = [
+    "As Kunkka watched the demons take his ships down one by one, he had the satisfaction of wearing away their fleet with his ancestral magic. Lord of Heaven, father of gods, Zeus treats all the Heroes as if they are his rambunctious, rebellious children.",
+    "The monks of Turstarkuri watched the rugged valleys below their mountain monastery as wave after wave of invaders swept through the lower kingdoms.",
+    "Among the emblematic powers that populate the Underscape, Razor the Lightning Revenant is one of the most feared. With his whip of lightning, he patrols the Narrow Maze, that treacherous webwork of passages by which the souls of the dead are sorted according to their own innate intelligence, cunning and persistence.",
+];
